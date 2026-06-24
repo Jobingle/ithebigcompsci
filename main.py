3ac58c8c76ba86ff50767ce06c4a088be0388760
jobingle <jonjonburrell@gmail.com>
2026-06-24 09:43:35 +0100

    swapsies

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
 import pygame
 from settings import *
-class startscr:
-    screen = pygame.display.set_mode((300, 300))
-
-    pygame.display.set_caption('Jon game')
-
-
-
-    # Variable to keep our game loop running
-    running = True
-
-    # game loop
-    while running:
-    
-    # for loop through the event queue  
-        for event in pygame.event.get():
-        
-            # Check for QUIT event      
-            if event.type == pygame.QUIT:
-                running = False
-        # Fill the background colour to the screen
-        screen.fill(background_colour)
-
-        # Update the display using flip
-        pygame.display.flip()
+from startscr import Startscr
