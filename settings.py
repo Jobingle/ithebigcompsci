191d76834debfed9e7ed4758c350faf3be840b41
jobingle <jonjonburrell@gmail.com>
2026-06-24 11:03:08 +0100

    yes

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
+import pygame, sys, random
+pygame.init()
 background_colour = (143,143,143)
 screen_width = 700
 screen_height = 500
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+TEXTCOLOUR = (200, 100, 0)
+FPS = 60
+fpsClock = pygame.time.Clock()
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300#
+font = pygame.font.Font(None, 50)
+fontObj = pygame.font.SysFont('courier', 35)
+surufaceObj = fontObj.render('Are you ready...', True, TEXTCOLOUR, None)
--- startscr.py
+++ startscr.py
@@ -1,27 +1,19 @@
 import pygame
-
+from game import *
 from settings import *
 class Startscr:
-    screen = pygame.display.set_mode((300, 300))
-
+    screen = pygame.display.set_mode((1950,1050))
     pygame.display.set_caption('Jon game')
-
-
-
     # Variable to keep our game loop running
     running = True
-
     # game loop
     while running:
-    
     # for loop through the event queue  
         for event in pygame.event.get():
-        
             # Check for QUIT event      
             if event.type == pygame.QUIT:
                 running = False
         # Fill the background colour to the screen
         screen.fill(background_colour)
-
-        # Update the display using flip
+        screen.blit(surufaceObj, (100, 100))
         pygame.display.flip()
